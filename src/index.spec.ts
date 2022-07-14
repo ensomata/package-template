b65791e3b2a1d5c108bcad5e26a7865a6facd6af
Philip Damra <pdamra@ensomata.com>
2022-07-13 17:34:09 -0700

    fix: added a test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,8 +1,16 @@
 import { hello } from '.';
 
 describe('hello function', () => {
+  it('should say hello', () => {
+    const expected = 'Hello';
+    expect(hello()).toMatch(expected);
+  });
   it('should include the passed in name', () => {
     const name = 'jimbo';
     expect(hello(name)).toMatch(name);
+  });
+  it('should use the default if no name is provided', () => {
+    const expected = 'world';
+    expect(hello()).toMatch(expected);
   });
 });
